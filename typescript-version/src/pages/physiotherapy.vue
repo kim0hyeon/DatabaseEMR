95c1449c60683e434e090ecaf1aad2205465619e
kim0hyeon <dbfwjs991105@gmail.com>
2023-11-25 03:08:28 +0900

    물리,재활치료 프레임

--- typescript-version/src/pages/physiotherapy.vue
+++ typescript-version/src/pages/physiotherapy.vue
@@ -5,31 +5,18 @@
 <template>
 
   <VRow>
-    <VCol cols="9">
-      <VCard title="진료내용">
-        <p style="text-align : center">진료 내용이 들어올 자리</p>
-      </VCard>
-    </VCol>
+    <VCol cols="12">
+        <VCard title="물리치료">
 
-    <VCol cols="3">
-      <VCard title="사진&영상" style="padding : 10px">
-        <p style="text-align : center">사진&영상 미리보기</p>
-        <VBtn class="mt-5 offset-md-8"> 업로드 </VBtn>
-      </VCard>
+        </VCard>
     </VCol>
   </VRow>
 
   <VRow>
-    <VCol cols="6">
-      <VCard title="진단">
-        <p style="text-align : center">진단 내용이 들어올 자리</p>
-      </VCard>
-    </VCol>
-    
-    <VCol cols="6">
-      <VCard title="처방">
-        <p style="text-align : center">처방 내용이 들어올 자리</p>
-      </VCard>
+    <VCol cols="12">
+        <VCard title="재활치료">
+            
+        </VCard>
     </VCol>
   </VRow>
 

053751ba81f93f441b5f396cafd4be8f725ea4ce
kim0hyeon <dbfwjs991105@gmail.com>
2023-12-14 20:12:32 +0900

    혈액검사 완성

--- typescript-version/src/views/pages/account-settings/BloodExamination.vue
+++ typescript-version/src/views/pages/account-settings/BloodExamination.vue
@@ -4,22 +4,26 @@ import { Chart, registerables } from 'chart.js'
 
 export default defineComponent({
   setup() {
-    const chartCanvas = ref<HTMLCanvasElement | null>(null)
+    const chart1 = ref<HTMLCanvasElement | null>(null)
+    const chart2 = ref<HTMLCanvasElement | null>(null)
+    const chart3 = ref<HTMLCanvasElement | null>(null)
+    const chart4 = ref<HTMLCanvasElement | null>(null)
+    const chart5 = ref<HTMLCanvasElement | null>(null)
 
     onMounted(() => {
-      if (chartCanvas.value) {
-        const ctx = chartCanvas.value.getContext('2d')
+      if (chart1.value) {
+        const ctx = chart1.value.getContext('2d')
         if (ctx) {
           Chart.register(...registerables)
 
           new Chart(ctx, {
-            type: 'bar',
+            type: 'bar', // 막대 그래프
             data: {
-              labels: ['혈액 검사 항목1', '혈액 검사 항목2', '혈액 검사 항목3'],
+              labels: ['혈색소'],
               datasets: [
                 {
-                  label: '혈액 검사 결과',
-                  data: [80, 60, 75], // 실제 데이터 입력
+                  label: '(g/dl)',
+                  data: [10], // 데이터 입력
                   backgroundColor: 'rgba(54, 162, 235, 0.2)',
                   borderColor: 'rgba(54, 162, 235, 1)',
                   borderWidth: 1,
@@ -27,10 +31,160 @@ export default defineComponent({
               ],
             },
             options: {
+              indexAxis: 'y', // 가로로 출력되도록 설정
               responsive: true,
               scales: {
                 x: {
-                  type: 'category',
+                  beginAtZero: true,
+                  min: 0,
+                  max: 20,
+                },
+                y: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          })
+        }
+      }
+      if (chart2.value) {
+        const ctx = chart2.value.getContext('2d')
+        if (ctx) {
+          Chart.register(...registerables)
+
+          new Chart(ctx, {
+            type: 'bar', // 막대 그래프
+            data: {
+              labels: ['공복혈당'],
+              datasets: [
+                {
+                  label: '(mg/dl)',
+                  data: [84], // 데이터 입력
+                  backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                  borderColor: 'rgba(54, 162, 235, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              indexAxis: 'y', // 가로로 출력되도록 설정
+              responsive: true,
+              scales: {
+                x: {
+                  beginAtZero: true,
+                  min: 0,
+                  max: 150,
+                },
+                y: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          })
+        }
+      }
+
+      if (chart3.value) {
+        const ctx = chart3.value.getContext('2d')
+        if (ctx) {
+          Chart.register(...registerables)
+
+          new Chart(ctx, {
+            type: 'bar', // 막대 그래프
+            data: {
+              labels: ['총콜레스테롤', 'HDL-콜레스테롤', '중성지방', 'LDL-콜레스테롤'],
+              datasets: [
+                {
+                  label: '(mg/dl)',
+                  data: [150, 72, 41, 69], // 데이터 입력
+                  backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                  borderColor: 'rgba(54, 162, 235, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              indexAxis: 'y', // 가로로 출력되도록 설정
+              responsive: true,
+              scales: {
+                x: {
+                  beginAtZero: true,
+                  min: 0,
+                  max: 250,
+                },
+                y: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          })
+        }
+      }
+
+      if (chart4.value) {
+        const ctx = chart4.value.getContext('2d')
+        if (ctx) {
+          Chart.register(...registerables)
+
+          new Chart(ctx, {
+            type: 'bar', // 막대 그래프
+            data: {
+              labels: ['혈청크레아티닌', '신사구체여과율(e-GFR)'],
+              datasets: [
+                {
+                  label: '(mg/dl)',
+                  data: [10, 111], // 데이터 입력
+                  backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                  borderColor: 'rgba(54, 162, 235, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              indexAxis: 'y', // 가로로 출력되도록 설정
+              responsive: true,
+              scales: {
+                x: {
+                  beginAtZero: true,
+                  min: 0,
+                  max: 160,
+                },
+                y: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          })
+        }
+      }
+
+      if (chart5.value) {
+        const ctx = chart5.value.getContext('2d')
+        if (ctx) {
+          Chart.register(...registerables)
+
+          new Chart(ctx, {
+            type: 'bar', // 막대 그래프
+            data: {
+              labels: ['AST(SGOT)', 'ALT(SGPT)', '감마지티피(v-GTP)'],
+              datasets: [
+                {
+                  label: '(U/L)',
+                  data: [39, 23, 24], // 데이터 입력
+                  backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                  borderColor: 'rgba(54, 162, 235, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              indexAxis: 'y', // 가로로 출력되도록 설정
+              responsive: true,
+              scales: {
+                x: {
+                  beginAtZero: true,
+                  min: 0,
+                  max: 200,
                 },
                 y: {
                   beginAtZero: true,
@@ -43,15 +197,17 @@ export default defineComponent({
     })
 
     return {
-      chartCanvas,
+      chart1,
+      chart2,
+      chart3,
+      chart4,
+      chart5,
     }
   },
 })
 </script>
 
 <template>
-  <canvas ref="chartCanvas"></canvas>
-
   <VRow>
     <VCol>
       <h2 style="margin-left: 10px; margin-bottom: 10px">Blood Test Result</h2>
@@ -63,38 +219,47 @@ export default defineComponent({
           <tr class="table-content">
             <th>빈혈</th>
             <td>
-              <p>혈색소 그래프1</p>
+              <canvas
+                ref="chart1"
+                height="45"
+              ></canvas>
             </td>
             <td></td>
           </tr>
           <tr>
             <th>당뇨병</th>
             <td>
-              <p>공복혈당 그래프1</p>
+              <canvas
+                ref="chart2"
+                height="45"
+              ></canvas>
             </td>
           </tr>
           <tr class="table-content">
             <th>이상지질혈증</th>
             <td>
-              <p>총 콜레스테롤 그래프1</p>
-              <p>HDL-콜레스테롤 그래프2</p>
-              <p>중성지방</p>
-              <p>LDL-콜레스테롤</p>
+              <canvas
+                ref="chart3"
+                height="120"
+              ></canvas>
             </td>
           </tr>
           <tr>
             <th>신장질환</th>
             <td>
-              <p>혈청 크레아티닌 그래프1</p>
-              <p>신사구체여과율(e-GFR) 그래프2</p>
+              <canvas
+                ref="chart4"
+                height="70"
+              ></canvas>
             </td>
           </tr>
           <tr class="table-content">
             <th>간장질환</th>
             <td>
-              <p>AST(SGOT)</p>
-              <p>ALT(SGPT)</p>
-              <p>감마지티피(v-GTP)</p>
+              <canvas
+                ref="chart5"
+                height="100"
+              ></canvas>
             </td>
           </tr>
         </table>

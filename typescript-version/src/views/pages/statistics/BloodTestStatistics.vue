c6955ccbd98b22901b5e8632aad3938e6a3b4a59
kim0hyeon <dbfwjs991105@gmail.com>
2023-12-17 17:59:03 +0900

    통계 페이지 완성!

--- typescript-version/src/views/pages/statistics/BloodTestStatistics.vue
+++ typescript-version/src/views/pages/statistics/BloodTestStatistics.vue
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useRoute } from 'vue-router'
 import { ref, onMounted } from 'vue'
 import Chart from 'chart.js/auto'
+import { IdStore } from '@/store'
 
 interface BloodTest {
   hemoglobin: number
@@ -16,10 +17,12 @@ interface BloodTest {
   ast: number
   alt: number
   gamma_gt: number
-  date_time: string
+  record_date: string
+  patient_id: string
+  blood_id: string
 }
 
-const bloodTests: BloodTest[] = [
+const BloodTests: BloodTest[] = [
   {
     hemoglobin: 14,
     fasting_blood_sugar: 90,
@@ -32,7 +35,9 @@ const bloodTests: BloodTest[] = [
     ast: 25,
     alt: 30,
     gamma_gt: 20,
-    date_time: '2023-12-04',
+    record_date: '2023-12-04',
+    patient_id: '12312',
+    blood_id: '123123',
   },
   {
     hemoglobin: 13.5,
@@ -46,7 +51,9 @@ const bloodTests: BloodTest[] = [
     ast: 30,
     alt: 35,
     gamma_gt: 22,
-    date_time: '2023-12-06',
+    record_date: '2023-12-06',
+    patient_id: '12312',
+    blood_id: '123123',
   },
   {
     hemoglobin: 15,
@@ -60,7 +67,9 @@ const bloodTests: BloodTest[] = [
     ast: 28,
     alt: 32,
     gamma_gt: 18,
-    date_time: '2023-12-08',
+    record_date: '2023-12-08',
+    patient_id: '12312',
+    blood_id: '123123',
   },
   {
     hemoglobin: 14.2,
@@ -74,7 +83,9 @@ const bloodTests: BloodTest[] = [
     ast: 32,
     alt: 38,
     gamma_gt: 24,
-    date_time: '2023-12-10',
+    record_date: '2023-12-10',
+    patient_id: '12312',
+    blood_id: '123123',
   },
   {
     hemoglobin: 14.5,
@@ -88,9 +99,28 @@ const bloodTests: BloodTest[] = [
     ast: 27,
     alt: 34,
     gamma_gt: 21,
-    date_time: '2023-12-12',
+    record_date: '2023-12-12',
+    patient_id: '12312',
+    blood_id: '123123',
   },
 ]
+
+// 백엔드에서 환자 정보 받아오기
+const token = sessionStorage.getItem('token')
+const store = IdStore()
+let BloodInformation = ref<BloodTest[]>([])
+const loadBloodData = async () => {
+  console.log('load blood data')
+  try {
+    const response = await axios.get(`http://yunsseong.uk:8000/api/blood?patient=${store.id}`, {
+      headers: { Authorization: `Token ${token}` },
+    })
+    BloodInformation.value = response.data
+    console.log(BloodInformation.value[0])
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 // 차트 그래프 만들기
 export default {
@@ -109,6 +139,18 @@ export default {
     const CheckALT = ref(false)
     const CheckGG = ref(false)
 
+    // patient.id가 바뀌었을 경우 실행할 로직
+    watch(store, (newValue, oldValue) => {
+      loadBloodData()
+      console.log('patient.id값이 변경되었습니다.')
+    })
+
+    // 다른 환자를 선택했을 때 실행할 함수 2개
+    const runMethods = () => {
+      loadBloodData()
+      updateChart()
+    }
+
     // 그래프를 그리는 함수
     const drawChart = () => {
       if (chart.value) {
@@ -119,11 +161,11 @@ export default {
             type: 'line',
             data: {
               labels: [
-                bloodTests[0].date_time,
-                bloodTests[1].date_time,
-                bloodTests[2].date_time,
-                bloodTests[3].date_time,
-                bloodTests[4].date_time,
+                BloodTests[0].record_date,
+                BloodTests[1].record_date,
+                BloodTests[2].record_date,
+                BloodTests[3].record_date,
+                BloodTests[4].record_date,
               ],
               datasets: [
                 {
@@ -149,6 +191,7 @@ export default {
     }
 
     onMounted(() => {
+      loadBloodData()
       drawChart()
     })
 
@@ -161,11 +204,11 @@ export default {
           label: '혈색소',
           borderColor: 'rgb(75, 192, 192)',
           data: [
-            bloodTests[0].hemoglobin,
-            bloodTests[1].hemoglobin,
-            bloodTests[2].hemoglobin,
-            bloodTests[3].hemoglobin,
-            bloodTests[4].hemoglobin,
+            BloodInformation.value[0].hemoglobin,
+            BloodInformation.value[1].hemoglobin,
+            BloodInformation.value[2].hemoglobin,
+            BloodInformation.value[3].hemoglobin,
+            BloodInformation.value[4].hemoglobin,
           ],
         }
         dataset.push(datas)
@@ -175,11 +218,11 @@ export default {
           label: '공복혈당',
           borderColor: 'rgb(255, 99, 132)',
           data: [
-            bloodTests[0].fasting_blood_sugar,
-            bloodTests[1].fasting_blood_sugar,
-            bloodTests[2].fasting_blood_sugar,
-            bloodTests[3].fasting_blood_sugar,
-            bloodTests[4].fasting_blood_sugar,
+            BloodInformation.value[0].fasting_blood_sugar,
+            BloodInformation.value[1].fasting_blood_sugar,
+            BloodInformation.value[2].fasting_blood_sugar,
+            BloodInformation.value[3].fasting_blood_sugar,
+            BloodInformation.value[4].fasting_blood_sugar,
           ],
         }
         dataset.push(datas)
@@ -189,11 +232,11 @@ export default {
           label: '총콜레스테롤',
           borderColor: 'rgb(79, 99, 132)',
           data: [
-            bloodTests[0].total_cholesterol,
-            bloodTests[1].total_cholesterol,
-            bloodTests[2].total_cholesterol,
-            bloodTests[3].total_cholesterol,
-            bloodTests[4].total_cholesterol,
+            BloodInformation.value[0].total_cholesterol,
+            BloodInformation.value[1].total_cholesterol,
+            BloodInformation.value[2].total_cholesterol,
+            BloodInformation.value[3].total_cholesterol,
+            BloodInformation.value[4].total_cholesterol,
           ],
         }
         dataset.push(datas)
@@ -203,11 +246,11 @@ export default {
           label: 'HDL-콜레스테롤',
           borderColor: 'rgb(200, 50, 100)',
           data: [
-            bloodTests[0].hdl_cholesterol,
-            bloodTests[1].hdl_cholesterol,
-            bloodTests[2].hdl_cholesterol,
-            bloodTests[3].hdl_cholesterol,
-            bloodTests[4].hdl_cholesterol,
+            BloodInformation.value[0].hdl_cholesterol,
+            BloodInformation.value[1].hdl_cholesterol,
+            BloodInformation.value[2].hdl_cholesterol,
+            BloodInformation.value[3].hdl_cholesterol,
+            BloodInformation.value[4].hdl_cholesterol,
           ],
         }
         dataset.push(datas)
@@ -217,11 +260,11 @@ export default {
           label: '중성지방',
           borderColor: 'rgb(150, 200, 50)',
           data: [
-            bloodTests[0].triglycerides,
-            bloodTests[1].triglycerides,
-            bloodTests[2].triglycerides,
-            bloodTests[3].triglycerides,
-            bloodTests[4].triglycerides,
+            BloodInformation.value[0].triglycerides,
+            BloodInformation.value[1].triglycerides,
+            BloodInformation.value[2].triglycerides,
+            BloodInformation.value[3].triglycerides,
+            BloodInformation.value[4].triglycerides,
           ],
         }
         dataset.push(datas)
@@ -231,11 +274,11 @@ export default {
           label: 'LDL-콜레스테롤',
           borderColor: 'rgb(100, 150, 200)',
           data: [
-            bloodTests[0].ldl_cholesterol,
-            bloodTests[1].ldl_cholesterol,
-            bloodTests[2].ldl_cholesterol,
-            bloodTests[3].ldl_cholesterol,
-            bloodTests[4].ldl_cholesterol,
+            BloodInformation.value[0].ldl_cholesterol,
+            BloodInformation.value[1].ldl_cholesterol,
+            BloodInformation.value[2].ldl_cholesterol,
+            BloodInformation.value[3].ldl_cholesterol,
+            BloodInformation.value[4].ldl_cholesterol,
           ],
         }
         dataset.push(datas)
@@ -245,11 +288,11 @@ export default {
           label: '혈청크레아티닌',
           borderColor: 'rgb(50, 100, 200)',
           data: [
-            bloodTests[0].serum_creatinine,
-            bloodTests[1].serum_creatinine,
-            bloodTests[2].serum_creatinine,
-            bloodTests[3].serum_creatinine,
-            bloodTests[4].serum_creatinine,
+            BloodInformation.value[0].serum_creatinine,
+            BloodInformation.value[1].serum_creatinine,
+            BloodInformation.value[2].serum_creatinine,
+            BloodInformation.value[3].serum_creatinine,
+            BloodInformation.value[4].serum_creatinine,
           ],
         }
         dataset.push(datas)
@@ -259,11 +302,11 @@ export default {
           label: '신사구체여과율(e-GFR)',
           borderColor: 'rgb(220, 120, 40)',
           data: [
-            bloodTests[0].glomerular_filtration_rate,
-            bloodTests[1].glomerular_filtration_rate,
-            bloodTests[2].glomerular_filtration_rate,
-            bloodTests[3].glomerular_filtration_rate,
-            bloodTests[4].glomerular_filtration_rate,
+            BloodInformation.value[0].glomerular_filtration_rate,
+            BloodInformation.value[1].glomerular_filtration_rate,
+            BloodInformation.value[2].glomerular_filtration_rate,
+            BloodInformation.value[3].glomerular_filtration_rate,
+            BloodInformation.value[4].glomerular_filtration_rate,
           ],
         }
         dataset.push(datas)
@@ -272,7 +315,13 @@ export default {
         const datas = {
           label: 'AST(SGOT)',
           borderColor: 'rgb(30, 180, 220)',
-          data: [bloodTests[0].ast, bloodTests[1].ast, bloodTests[2].ast, bloodTests[3].ast, bloodTests[4].ast],
+          data: [
+            BloodInformation.value[0].ast,
+            BloodInformation.value[1].ast,
+            BloodInformation.value[2].ast,
+            BloodInformation.value[3].ast,
+            BloodInformation.value[4].ast,
+          ],
         }
         dataset.push(datas)
       }
@@ -280,7 +329,13 @@ export default {
         const datas = {
           label: 'ALT(SGPT)',
           borderColor: 'rgb(30, 140, 170)',
-          data: [bloodTests[0].alt, bloodTests[1].alt, bloodTests[2].alt, bloodTests[3].alt, bloodTests[4].alt],
+          data: [
+            BloodInformation.value[0].alt,
+            BloodInformation.value[1].alt,
+            BloodInformation.value[2].alt,
+            BloodInformation.value[3].alt,
+            BloodInformation.value[4].alt,
+          ],
         }
         dataset.push(datas)
       }
@@ -289,11 +344,11 @@ export default {
           label: '감마지티피(v-GTP)',
           borderColor: 'rgb(30, 140, 170)',
           data: [
-            bloodTests[0].gamma_gt,
-            bloodTests[1].gamma_gt,
-            bloodTests[2].gamma_gt,
-            bloodTests[3].gamma_gt,
-            bloodTests[4].gamma_gt,
+            BloodInformation.value[0].gamma_gt,
+            BloodInformation.value[1].gamma_gt,
+            BloodInformation.value[2].gamma_gt,
+            BloodInformation.value[3].gamma_gt,
+            BloodInformation.value[4].gamma_gt,
           ],
         }
         dataset.push(datas)
@@ -313,11 +368,11 @@ export default {
             type: 'line',
             data: {
               labels: [
-                bloodTests[0].date_time,
-                bloodTests[1].date_time,
-                bloodTests[2].date_time,
-                bloodTests[3].date_time,
-                bloodTests[4].date_time,
+                BloodInformation.value[0].record_date.slice(0, 10),
+                BloodInformation.value[1].record_date.slice(0, 10),
+                BloodInformation.value[2].record_date.slice(0, 10),
+                BloodInformation.value[3].record_date.slice(0, 10),
+                BloodInformation.value[4].record_date.slice(0, 10),
               ],
               datasets: dataset,
             },
@@ -340,40 +395,12 @@ export default {
       CheckALT,
       CheckGG,
       updateChart,
+      runMethods,
     }
   },
 }
 
 const route = useRoute()
-
-// 혈액검사 테스트 기록 정보 받아오기 (속성들은 이름 정하면 그때 넣기)
-interface BloodTest {
-  hemoglobin: number
-  fasting_blood_sugar: number
-  total_cholesterol: number
-  hdl_cholesterol: number
-  triglycerides: number
-  ldl_cholesterol: number
-  serum_creatinine: number
-  glomerular_filtration_rate: number
-  ast: number
-  alt: number
-  gamma_gt: number
-}
-
-// 혈액검사기록을 저장할 리스트 생성 -> 변경 가능해야함
-let BloodTestRecord = ref<BloodTest[]>([])
-
-// 오류나지 않게 임시로 url 넣어놨음. 혈액검사에 맞는 url로 고쳐줘야함
-onMounted(async () => {
-  try {
-    const response = await axios.get('http;//yunsseong.uk:8000/api/patients/') // 수정 필요
-    const BloodTestRecord = response.data // 가져온 데이터를 어떻게 가공하여 사용할지 정의 필요
-    console.log('Blood test record loading success')
-  } catch (error) {
-    console.error(error)
-  }
-})
 </script>
 <template>
   <VRow>
@@ -440,7 +467,7 @@ onMounted(async () => {
             class="text-center"
             style="margin: 10px"
           >
-            <VBtn @click="updateChart">선택!</VBtn>
+            <VBtn @click="runMethods">선택!</VBtn>
           </div>
         </div>
       </VCard>
--- typescript-version/src/views/pages/statistics/InBodyTestStatistics.vue
+++ typescript-version/src/views/pages/statistics/InBodyTestStatistics.vue
@@ -3,10 +3,13 @@ import axios from 'axios'
 import { useRoute } from 'vue-router'
 import { ref, onMounted } from 'vue'
 import Chart from 'chart.js/auto'
+import { IdStore } from '@/store'
 
 // 인바디 테스트 기록 정보 받아오기 (속성들은 이름 정하면 그때 넣기)
 interface InBodyTest {
-  date_time: string
+  record_date: string
+  patient_id: string
+  inbody_id: string
   weight: number
   muscle_mass: number
   body_fat_mass: number
@@ -20,9 +23,11 @@ interface InBodyTest {
 }
 
 // 인바디기록을 저장할 리스트 생성 -> 쓰레기값
-const inBodyTests: InBodyTest[] = [
+const InBodyTests: InBodyTest[] = [
   {
-    date_time: '2023-12-04',
+    patient_id: 'asfdasdf',
+    inbody_id: 'asdfasdf',
+    record_date: '2023-12-04',
     weight: 70,
     muscle_mass: 32,
     body_fat_mass: 16,
@@ -35,7 +40,9 @@ const inBodyTests: InBodyTest[] = [
     left_leg: 21,
   },
   {
-    date_time: '2023-12-06',
+    patient_id: 'asfdasdf',
+    inbody_id: 'asdfasdf',
+    record_date: '2023-12-06',
     weight: 75,
     muscle_mass: 35,
     body_fat_mass: 18,
@@ -48,7 +55,9 @@ const inBodyTests: InBodyTest[] = [
     left_leg: 23,
   },
   {
-    date_time: '2023-12-08',
+    patient_id: 'asfdasdf',
+    inbody_id: 'asdfasdf',
+    record_date: '2023-12-08',
     weight: 85,
     muscle_mass: 40,
     body_fat_mass: 22,
@@ -61,7 +70,9 @@ const inBodyTests: InBodyTest[] = [
     left_leg: 27,
   },
   {
-    date_time: '2023-12-10',
+    patient_id: 'asfdasdf',
+    inbody_id: 'asdfasdf',
+    record_date: '2023-12-10',
     weight: 65,
     muscle_mass: 30,
     body_fat_mass: 15,
@@ -74,7 +85,9 @@ const inBodyTests: InBodyTest[] = [
     left_leg: 19,
   },
   {
-    date_time: '2023-12-12',
+    patient_id: 'asfdasdf',
+    inbody_id: 'asdfasdf',
+    record_date: '2023-12-12',
     weight: 80,
     muscle_mass: 38,
     body_fat_mass: 20,
@@ -87,6 +100,22 @@ const inBodyTests: InBodyTest[] = [
     left_leg: 25,
   },
 ]
+const token = sessionStorage.getItem('token')
+const store = IdStore()
+// 백엔드에서 환자 정보 받아오기
+let InBodyInformation = ref<InBodyTest[]>([])
+const loadInBodyData = async () => {
+  console.log('load inbody data')
+  try {
+    const response = await axios.get(`http://yunsseong.uk:8000/api/inbody?patient=${store.id}`, {
+      headers: { Authorization: `Token ${token}` },
+    })
+    InBodyInformation.value = response.data
+    console.log(InBodyInformation.value[0].weight)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 // 그래프 만들고 없애기
 export default {
@@ -103,6 +132,17 @@ export default {
     const isCheckedRL = ref(false)
     const isCheckedLL = ref(false)
 
+    // patient.id가 바뀌었을 경우 실행할 로직
+    watch(store, (newValue, oldValue) => {
+      loadInBodyData()
+      console.log('patient.id값이 변경되었습니다.')
+    })
+
+    const runMethods = () => {
+      loadInBodyData()
+      updateChart()
+    }
+
     // 그래프를 그리는 함수
     const drawChart = () => {
       if (chart.value) {
@@ -113,11 +153,11 @@ export default {
             type: 'line',
             data: {
               labels: [
-                inBodyTests[0].date_time,
-                inBodyTests[1].date_time,
-                inBodyTests[2].date_time,
-                inBodyTests[3].date_time,
-                inBodyTests[4].date_time,
+                InBodyTests[0].record_date,
+                InBodyTests[1].record_date,
+                InBodyTests[2].record_date,
+                InBodyTests[3].record_date,
+                InBodyTests[4].record_date,
               ],
               datasets: [
                 {
@@ -143,6 +183,7 @@ export default {
     }
 
     onMounted(() => {
+      loadInBodyData()
       drawChart()
     })
 
@@ -155,11 +196,11 @@ export default {
           label: '체중',
           borderColor: 'rgb(75, 192, 192)',
           data: [
-            inBodyTests[0].weight,
-            inBodyTests[1].weight,
-            inBodyTests[2].weight,
-            inBodyTests[3].weight,
-            inBodyTests[4].weight,
+            InBodyInformation.value[0].weight,
+            InBodyInformation.value[1].weight,
+            InBodyInformation.value[2].weight,
+            InBodyInformation.value[3].weight,
+            InBodyInformation.value[4].weight,
           ],
         }
         dataset.push(datas)
@@ -169,11 +210,11 @@ export default {
           label: '근육량',
           borderColor: 'rgb(255, 99, 132)',
           data: [
-            inBodyTests[0].muscle_mass,
-            inBodyTests[1].muscle_mass,
-            inBodyTests[2].muscle_mass,
-            inBodyTests[3].muscle_mass,
-            inBodyTests[4].muscle_mass,
+            InBodyInformation.value[0].muscle_mass,
+            InBodyInformation.value[1].muscle_mass,
+            InBodyInformation.value[2].muscle_mass,
+            InBodyInformation.value[3].muscle_mass,
+            InBodyInformation.value[4].muscle_mass,
           ],
         }
         dataset.push(datas)
@@ -183,11 +224,11 @@ export default {
           label: '체지방량',
           borderColor: 'rgb(79, 99, 132)',
           data: [
-            inBodyTests[0].body_fat_mass,
-            inBodyTests[1].body_fat_mass,
-            inBodyTests[2].body_fat_mass,
-            inBodyTests[3].body_fat_mass,
-            inBodyTests[4].body_fat_mass,
+            InBodyInformation.value[0].body_fat_mass,
+            InBodyInformation.value[1].body_fat_mass,
+            InBodyInformation.value[2].body_fat_mass,
+            InBodyInformation.value[3].body_fat_mass,
+            InBodyInformation.value[4].body_fat_mass,
           ],
         }
         dataset.push(datas)
@@ -196,7 +237,13 @@ export default {
         const datas = {
           label: 'BMI',
           borderColor: 'rgb(200, 50, 100)',
-          data: [inBodyTests[0].bmi, inBodyTests[1].bmi, inBodyTests[2].bmi, inBodyTests[3].bmi, inBodyTests[4].bmi],
+          data: [
+            InBodyInformation.value[0].bmi,
+            InBodyInformation.value[1].bmi,
+            InBodyInformation.value[2].bmi,
+            InBodyInformation.value[3].bmi,
+            InBodyInformation.value[4].bmi,
+          ],
         }
         dataset.push(datas)
       }
@@ -205,11 +252,11 @@ export default {
           label: '체지방률',
           borderColor: 'rgb(150, 200, 50)',
           data: [
-            inBodyTests[0].percent_body_fat,
-            inBodyTests[1].percent_body_fat,
-            inBodyTests[2].percent_body_fat,
-            inBodyTests[3].percent_body_fat,
-            inBodyTests[4].percent_body_fat,
+            InBodyInformation.value[0].percent_body_fat,
+            InBodyInformation.value[1].percent_body_fat,
+            InBodyInformation.value[2].percent_body_fat,
+            InBodyInformation.value[3].percent_body_fat,
+            InBodyInformation.value[4].percent_body_fat,
           ],
         }
         dataset.push(datas)
@@ -219,11 +266,11 @@ export default {
           label: '오른팔',
           borderColor: 'rgb(100, 150, 200)',
           data: [
-            inBodyTests[0].right_arm,
-            inBodyTests[1].right_arm,
-            inBodyTests[2].right_arm,
-            inBodyTests[3].right_arm,
-            inBodyTests[4].right_arm,
+            InBodyInformation.value[0].right_arm,
+            InBodyInformation.value[1].right_arm,
+            InBodyInformation.value[2].right_arm,
+            InBodyInformation.value[3].right_arm,
+            InBodyInformation.value[4].right_arm,
           ],
         }
         dataset.push(datas)
@@ -233,11 +280,11 @@ export default {
           label: '왼팔',
           borderColor: 'rgb(50, 100, 200)',
           data: [
-            inBodyTests[0].left_arm,
-            inBodyTests[1].left_arm,
-            inBodyTests[2].left_arm,
-            inBodyTests[3].left_arm,
-            inBodyTests[4].left_arm,
+            InBodyInformation.value[0].left_arm,
+            InBodyInformation.value[1].left_arm,
+            InBodyInformation.value[2].left_arm,
+            InBodyInformation.value[3].left_arm,
+            InBodyInformation.value[4].left_arm,
           ],
         }
         dataset.push(datas)
@@ -247,11 +294,11 @@ export default {
           label: '몸통',
           borderColor: 'rgb(220, 120, 40)',
           data: [
-            inBodyTests[0].trunk,
-            inBodyTests[1].trunk,
-            inBodyTests[2].trunk,
-            inBodyTests[3].trunk,
-            inBodyTests[4].trunk,
+            InBodyInformation.value[0].trunk,
+            InBodyInformation.value[1].trunk,
+            InBodyInformation.value[2].trunk,
+            InBodyInformation.value[3].trunk,
+            InBodyInformation.value[4].trunk,
           ],
         }
         dataset.push(datas)
@@ -261,11 +308,11 @@ export default {
           label: '오른다리',
           borderColor: 'rgb(30, 180, 220)',
           data: [
-            inBodyTests[0].right_leg,
-            inBodyTests[1].right_leg,
-            inBodyTests[2].right_leg,
-            inBodyTests[3].right_leg,
-            inBodyTests[4].right_leg,
+            InBodyInformation.value[0].right_leg,
+            InBodyInformation.value[1].right_leg,
+            InBodyInformation.value[2].right_leg,
+            InBodyInformation.value[3].right_leg,
+            InBodyInformation.value[4].right_leg,
           ],
         }
         dataset.push(datas)
@@ -275,16 +322,17 @@ export default {
           label: '왼다리',
           borderColor: 'rgb(30, 140, 170)',
           data: [
-            inBodyTests[0].left_leg,
-            inBodyTests[1].left_leg,
-            inBodyTests[2].left_leg,
-            inBodyTests[3].left_leg,
-            inBodyTests[4].left_leg,
+            InBodyInformation.value[0].left_leg,
+            InBodyInformation.value[1].left_leg,
+            InBodyInformation.value[2].left_leg,
+            InBodyInformation.value[3].left_leg,
+            InBodyInformation.value[4].left_leg,
           ],
         }
         dataset.push(datas)
       }
-      // 차트 파괴하기
+
+      // 기존차트 파괴하기
       const existingChart = chart.value ? Chart.getChart(chart.value) : null
       if (existingChart) {
         existingChart.destroy()
@@ -299,11 +347,11 @@ export default {
             type: 'line',
             data: {
               labels: [
-                inBodyTests[0].date_time,
-                inBodyTests[1].date_time,
-                inBodyTests[2].date_time,
-                inBodyTests[3].date_time,
-                inBodyTests[4].date_time,
+                InBodyInformation.value[0].record_date.slice(0, 10),
+                InBodyInformation.value[1].record_date.slice(0, 10),
+                InBodyInformation.value[2].record_date.slice(0, 10),
+                InBodyInformation.value[3].record_date.slice(0, 10),
+                InBodyInformation.value[4].record_date.slice(0, 10),
               ],
               datasets: dataset,
             },
@@ -324,9 +372,9 @@ export default {
       isCheckedT,
       isCheckedRL,
       isCheckedLL,
-      inBodyTests,
       updateChart,
       drawChart,
+      runMethods,
     }
   },
 }
@@ -387,7 +435,7 @@ const route = useRoute()
             class="text-center"
             style="margin: 10px"
           >
-            <VBtn @click="updateChart">선택!</VBtn>
+            <VBtn @click="runMethods">선택!</VBtn>
           </div>
         </div>
       </VCard>
